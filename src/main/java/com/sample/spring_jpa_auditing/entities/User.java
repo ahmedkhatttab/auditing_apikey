mayank042/spring-jpa-auditing

initial commit - full source

src/main/java/com/sample/spring_jpa_auditing/entities/User.java
@@ -1,0 +1,34 @@
+package com.sample.spring_jpa_auditing.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Accessors(chain = true)
+@Entity
+@Table(name = "user")
+public class User extends AuditorEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id", nullable = false)
+    private Long id;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Phone")
+    private String phone;
+}

ahmedkhatttab/auditing_apikey

src/main/java/com/sample/spring_jpa_auditing/entities/User.java
@@ -1,0 +1,34 @@
+package com.sample.spring_jpa_auditing.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Accessors(chain = true)
+@Entity
+@Table(name = "user_tb")
+public class User extends AuditorEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id", nullable = false)
+    private Long id;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Phone")
+    private String phone;
+}